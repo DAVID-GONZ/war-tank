67d72dd63361124e2af70d858459bf09158cbfba
DAVID-GONZ <85770465+DAVID-GONZ@users.noreply.github.com>
2022-10-06 17:02:06 -0500

    Change direction of main movement

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 basic.forever(function () {
-    if (mbit_Robot.Avoid_Sensor(mbit_Robot.enAvoidState.OBSTACLE)) {
+    if (mbit_Robot.Avoid_Sensor(mbit_Robot.enAvoidState.NOOBSTACLE)) {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
+    } else {
         mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Back)
         basic.pause(1000)
-        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinRight)
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinLeft)
         basic.pause(1000)
-    } else {
-        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
     }
 })
